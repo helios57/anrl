cc8d4e2ba9985abf1e880ab47f780b3777035ba0
helios157 <helios157@3ecfba0c-028a-11de-ada4-5153b8187bf2>
2009-03-03 16:07:56 +0000

    Upload Initial Project
    git-svn-id: https://anrl.googlecode.com/svn/trunk@21 3ecfba0c-028a-11de-ada4-5153b8187bf2

---
+++ AirNavigationRaceLive/AirNavigationRaceLive/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Air Navigation Race Live")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("way-up @ gibb.ch")]
+[assembly: AssemblyProduct("AirNavigationRaceLive")]
+[assembly: AssemblyCopyright("OpenSource")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6027f389-e056-49f0-95fe-d3c6e9886401")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
