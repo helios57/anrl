7f93250c76c65b25d94eeae37d2dc7f59abcde6c
dominique.huber@gmx.ch <dominique.huber@gmx.ch@3ecfba0c-028a-11de-ada4-5153b8187bf2>
2009-03-21 20:33:47 +0000

    git-svn-id: https://anrl.googlecode.com/svn/trunk@42 3ecfba0c-028a-11de-ada4-5153b8187bf2

---
+++ AirNavigationRaceLive/GELive/GEToolStrip.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace GELive
+{
+    public partial class GEToolStrip : Component
+    {
+        public GEToolStrip()
+        {
+            InitializeComponent();
+        }
+
+        public GEToolStrip(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
--- AirNavigationRaceLive/GELive/anrl_gui.Designer.cs
+++ AirNavigationRaceLive/GELive/anrl_gui.Designer.cs
@@ -28,14 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(anrl_gui));
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.geWebBrowser1 = new GELive.GEWebBrowser();
-            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.LoadKml = new System.Windows.Forms.ToolStripButton();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
-            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // splitContainer1
@@ -64,42 +60,17 @@
             this.geWebBrowser1.TabIndex = 0;
             this.geWebBrowser1.WebBrowserShortcutsEnabled = false;
             // 
-            // toolStrip1
-            // 
-            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.LoadKml});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
-            this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(633, 25);
-            this.toolStrip1.TabIndex = 1;
-            this.toolStrip1.Text = "toolStrip1";
-            // 
-            // LoadKml
-            // 
-            this.LoadKml.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.LoadKml.Image = ((System.Drawing.Image)(resources.GetObject("LoadKml.Image")));
-            this.LoadKml.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.LoadKml.Name = "LoadKml";
-            this.LoadKml.Size = new System.Drawing.Size(87, 22);
-            this.LoadKml.Text = "Load KML-File";
-            this.LoadKml.Click += new System.EventHandler(this.LoadKml_Click);
-
-            // 
             // anrl_gui
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(633, 524);
-            this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.splitContainer1);
             this.Name = "anrl_gui";
             this.Text = "Form1";
             this.splitContainer1.Panel2.ResumeLayout(false);
             this.splitContainer1.ResumeLayout(false);
-            this.toolStrip1.ResumeLayout(false);
-            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -107,8 +78,6 @@
 
         private System.Windows.Forms.SplitContainer splitContainer1;
         private GELive.GEWebBrowser geWebBrowser1;
-        private System.Windows.Forms.ToolStrip toolStrip1;
-        private System.Windows.Forms.ToolStripButton LoadKml;
 
 
     }
