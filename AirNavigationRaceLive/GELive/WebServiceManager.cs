4698ba172b58dcaa02e878e627a7ae0248b172fb
dominique.huber@gmx.ch <dominique.huber@gmx.ch@3ecfba0c-028a-11de-ada4-5153b8187bf2>
2009-03-23 22:28:55 +0000

    git-svn-id: https://anrl.googlecode.com/svn/trunk@49 3ecfba0c-028a-11de-ada4-5153b8187bf2

---
+++ AirNavigationRaceLive/GELive/WebServiceManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GELive
+{
+    /// <summary>
+    /// WebServiceManager class provides following functionality:
+    /// - query WebService to request tracker-data from application server
+    /// - provice data in appropriate format for visualization
+    /// (- request caching)
+    /// (- authentication to the WebService)
+    /// </summary>
+    class WebServiceManager
+    {
+    }
+}
