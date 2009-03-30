3282ce230d4207e92cbb5c574262d7b2d7e45afd
dominique.huber@gmx.ch <dominique.huber@gmx.ch@3ecfba0c-028a-11de-ada4-5153b8187bf2>
2009-03-30 21:54:30 +0000

    KmlBuilder: this class will be used to convert the data from the databse into a xml stream [in progress] DomeTesting: console application to test my stuff [for the purpose of testing only, will be deleted]
    git-svn-id: https://anrl.googlecode.com/svn/trunk@57 3ecfba0c-028a-11de-ada4-5153b8187bf2

---
+++ AirNavigationRaceLive/DomeTesting/KmlBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+public class KmlBuilder
+{    
+    public XmlDocument BuildKml(int ID, int Flugzeug_ID, DateTime Timestamp, double Xstart, double Xend, double Ystart, double Yend, double Zstart, double Zend, DateTime Tstart, DateTime Tend, double Speed, int Pentalty)
+    {
+        // checking of parameters needed?
+
+        XmlDocument kml = new XmlDocument();
+        try
+        {
+            kml.Load("../../track.xml");
+        }
+        catch (Exception err)
+        {
+            Console.WriteLine(err.Message);
+        }
+        return kml;
+    }
+}
