6da0c005114fc9468993c7f5502bce50ce60474d
helios157 <helios157@3ecfba0c-028a-11de-ada4-5153b8187bf2>
2009-04-27 11:33:51 +0000

    git-svn-id: https://anrl.googlecode.com/svn/trunk@74 3ecfba0c-028a-11de-ada4-5153b8187bf2

---
+++ AirNavigationRaceLive/TCPReciever/Control.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TCPReciever
+{
+    class Control:System.Windows.Forms.Form
+    {
+        public delegate void RecievedGPS(string message);
+        Server server;
+        public RecievedGPS MyDelegate;
+        public bool running;
+
+        public Control()
+        {
+            running = true;
+            server = new Server();
+            server.MessageReceived += new TCPReciever.Server.MessageReceivedHandler(Message_Received);
+        }
+
+        void Message_Received(string message)
+        {
+            MyDelegate = new RecievedGPS(ProcessRecievedGPSData);  
+              // this.invoke (MyDelegate, new string[] { message });
+            MyDelegate.Invoke(message);
+        }
+
+        public void ProcessRecievedGPSData(string GPSData)
+        {
+            String[] GPScoords = GPSData.Split(',');
+
+           /* textBox1.Text += "\r\n";
+            foreach (String ss in GPScoords)
+            {
+                textBox1.Text += ss + "  ";
+            }*/
+        }
+    }
+}
