daba9a04ac4f8872298e4642c14093d29c315cb0
helios157 <helios157@3ecfba0c-028a-11de-ada4-5153b8187bf2>
2009-04-27 11:45:59 +0000

    git-svn-id: https://anrl.googlecode.com/svn/trunk@78 3ecfba0c-028a-11de-ada4-5153b8187bf2

--- AirNavigationRaceLive/TCPReciever/Control.cs
+++ AirNavigationRaceLive/TCPReciever/Control.cs
@@ -10,6 +10,7 @@ namespace TCPReciever
         public delegate void RecievedGPS(string message);
         Server server;
         public RecievedGPS MyDelegate;
+        private System.Windows.Forms.Button button1;
         public bool running;
 
         public Control()
@@ -17,6 +18,8 @@ namespace TCPReciever
             running = true;
             server = new Server();
             server.MessageReceived += new TCPReciever.Server.MessageReceivedHandler(Message_Received);
+            InitializeComponent();
+            Show();
         }
 
         void Message_Received(string message)
@@ -35,6 +38,35 @@ namespace TCPReciever
             {
                 textBox1.Text += ss + "  ";
             }*/
+        }
+
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(94, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Control
+            // 
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.button1);
+            this.Name = "Control";
+            this.ResumeLayout(false);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
