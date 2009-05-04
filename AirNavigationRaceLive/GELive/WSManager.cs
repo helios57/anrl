e813aeb4d129feebe15a48882f41cd3e7d836d3d
dominique.huber@gmx.ch <dominique.huber@gmx.ch@3ecfba0c-028a-11de-ada4-5153b8187bf2>
2009-05-04 12:32:14 +0000

    git-svn-id: https://anrl.googlecode.com/svn/trunk@101 3ecfba0c-028a-11de-ada4-5153b8187bf2

--- AirNavigationRaceLive/GELive/WSManager.cs
+++ AirNavigationRaceLive/GELive/WSManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Xml;
+using GELive.anrlWebService;
 
 namespace GELive
 {
@@ -14,6 +15,12 @@ namespace GELive
             //return kml.ToString();
             // kml file loaded by resource file instead of webservice
             // change this when webservice is implemented
+            GELive.anrlWebService.ANRLDataServiceClient client = new ANRLDataServiceClient();
+            
+            // trackerId as paramater
+            string kml = client.GetKml(1);
+
+            
             return GELive.Properties.Resources.track;
 
         }
