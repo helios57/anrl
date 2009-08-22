46ec3ac89dde6d7743975591e94a139638361c14
helios157 <helios157@3ecfba0c-028a-11de-ada4-5153b8187bf2>
2009-08-22 17:26:37 +0000

    git-svn-id: https://anrl.googlecode.com/svn/trunk@207 3ecfba0c-028a-11de-ada4-5153b8187bf2

---
+++ AirNavigationRaceLive/GELive/Parcours.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace GELive
+{
+    partial class Parcours
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstParcours = new System.Windows.Forms.ListBox();
+            this.btnAddParcour = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstParcours
+            // 
+            this.lstParcours.FormattingEnabled = true;
+            this.lstParcours.Location = new System.Drawing.Point(12, 12);
+            this.lstParcours.Name = "lstParcours";
+            this.lstParcours.Size = new System.Drawing.Size(250, 186);
+            this.lstParcours.TabIndex = 0;
+            // 
+            // btnAddParcour
+            // 
+            this.btnAddParcour.Location = new System.Drawing.Point(12, 204);
+            this.btnAddParcour.Name = "btnAddParcour";
+            this.btnAddParcour.Size = new System.Drawing.Size(250, 23);
+            this.btnAddParcour.TabIndex = 1;
+            this.btnAddParcour.Text = "Add Selected Parcour";
+            this.btnAddParcour.UseVisualStyleBackColor = true;
+            this.btnAddParcour.Click += new System.EventHandler(this.btnAddParcour_Click);
+            // 
+            // Parcours
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 233);
+            this.Controls.Add(this.btnAddParcour);
+            this.Controls.Add(this.lstParcours);
+            this.Name = "Parcours";
+            this.Text = "Parcours";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstParcours;
+        private System.Windows.Forms.Button btnAddParcour;
+    }
+}
---
+++ AirNavigationRaceLive/GELive/Parcours.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GELive
+{
+    public partial class Parcours : Form
+    {
+
+        public event EventHandler OnParcourOk;
+        public Parcours()
+        {
+            InitializeComponent();
+            lstParcours.Items.Clear();
+            //InformationPool.Client
+        }
+
+        private void btnAddParcour_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
