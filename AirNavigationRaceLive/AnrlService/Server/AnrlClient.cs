e432aecb914858f5afed92c5832eda6879d1c578
helios157 <helios157@3ecfba0c-028a-11de-ada4-5153b8187bf2>
2010-08-14 06:42:31 +0000

    Added empty methods
    git-svn-id: https://anrl.googlecode.com/svn/trunk@311 3ecfba0c-028a-11de-ada4-5153b8187bf2

--- AirNavigationRaceLive/AnrlService/Server/AnrlClient.cs
+++ AirNavigationRaceLive/AnrlService/Server/AnrlClient.cs
@@ -8,5 +8,88 @@ namespace AnrlService.Server
 {
     class AnrlClient:IAnrlClient
     {
+        #region IAnrlClient Members
+
+        public List<ITracker> getTrackers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<IPilot> getPilots()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ITeam> getTeams()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<IRace> getRaces()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<IPenaltyZone> getPenaltyzones()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<IData> getData(List<ITracker> trackers, DateTime from, DateTime to)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long addPilot(IPilot pilot)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long addTeam(ITeam team)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long addRace(IRace race)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long addPenaltyZone(IPenaltyZone penaltyzone)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long addPicture(IPicture picture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool removePilot(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool removeTeam(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool removeRace(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool removePenaltyZone(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool removePicture(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
     }
 }
