cc79ff9ddc654f4fbae5d4ed1c083cce84a0162b
helios157 <helios157@3ecfba0c-028a-11de-ada4-5153b8187bf2>
2010-12-15 22:38:37 +0000

    git-svn-id: https://anrl.googlecode.com/svn/trunk@325 3ecfba0c-028a-11de-ada4-5153b8187bf2

---
+++ AirNavigationRaceLive/TestApplikation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AnrlInterfaces;
+
+namespace TestApplikation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parcour p = new Parcour();
+            Line newLine = new Line();
+            newLine.Point.Add(new Point());
+            p.EndPoint.Add(newLine);
+            System.Console.Out.WriteLine(p.Serialize());
+            System.Console.Read();
+        }
+    }
+}
